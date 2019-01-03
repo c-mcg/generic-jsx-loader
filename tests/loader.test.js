22f2d6e6e967ff85fea11893314a7ecf8d2d5e3f
Nicole <niknakbakpak@gmail.com>
2019-01-03 14:40:45 -0500

    Replaced basic test with first real test

--- tests/loader.test.js
+++ tests/loader.test.js
@@ -1,10 +1,33 @@
+import loaderUtils from 'loader-utils';
+
 import loader from "../src/loader"
 
 describe("generic-jsx-loader", () => {
 
-    it("Exports a function that returns a string", () => {
-        expect(loader).toBeInstanceOf(Function);
-        expect(loader()).toBeInstanceOf(String);
-    })
+    const oldGetOptions = loaderUtils.getOptions;
 
-})
+    beforeAll(() => {
+        loaderUtils.getOptions = jest.fn().mockReturnValue({ serialize: () => {} })
+    });
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    afterAll(() => {
+        loaderUtils.getOptions = oldGetOptions;
+    });
+
+    it("requires a serialize option", () => {
+        loaderUtils.getOptions.mockReturnValueOnce({});
+        expect(() => {
+            callLoaderWithSource()
+        }).toThrow();
+        expect(loaderUtils.getOptions).toHaveBeenCalled();
+    });
+
+});
+
+function callLoaderWithSource() {
+    return loader("test");
+}
