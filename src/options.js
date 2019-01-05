8243335b09b6cf213a19a89389675e173ec3ed08
Nicole <niknakbakpak@gmail.com>
2019-01-05 10:43:19 -0500

    Added options

--- src/options.js
+++ src/options.js
@@ -6,64 +6,8 @@ export default {
             instanceof: 'Function'
         },
     },
-    errorMessage: {},
+    errorMessage: {
+        serialize: "should be {String} or {Function} (https://github.com/nik-m2/generic-jsx-loader#serialize)",
+    },
     additionalProperties: false,
-/***********************************/
-/***********************************/
-// Copied from https://github.com
-//   /webpack-contrib/file-loader
-// for reference while developing!
-/***********************************/
-/***********************************/
-    // "properties": {
-    //     "name": {
-    //         "anyOf": [
-    //             {
-    //                 "type": "string"
-    //             },
-    //             {
-    //                 "instanceof": "Function"
-    //             }
-    //         ]
-    //     },
-    //     "outputPath": {
-    //         "anyOf": [
-    //             {
-    //                 "type": "string"
-    //             },
-    //             {
-    //                 "instanceof": "Function"
-    //             }
-    //         ]
-    //     },
-    //     "publicPath": {
-    //         "anyOf": [
-    //             {
-    //                 "type": "string"
-    //             },
-    //             {
-    //                 "instanceof": "Function"
-    //             }
-    //         ]
-    //     },
-    //     "context": {
-    //         "type": "string"
-    //     },
-    //     "emitFile": {
-    //         "type": "boolean"
-    //     },
-    //     "regExp": {
-    //         "anyOf": [
-    //             {
-    //                 "type": "string"
-    //             },
-    //             {
-    //                 "instanceof": "RegExp"
-    //             }
-    //         ]
-    //     }
-    // },
-
-    // Error messages
-    //     "publicPath": "should be {String} or {Function} (https://github.com/webpack-contrib/file-loader#publicpath)",
 };
