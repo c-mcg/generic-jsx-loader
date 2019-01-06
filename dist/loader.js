94eda1595109f3ded84ddfe64532f69b3cb9635f
Nicole <niknakbakpak@gmail.com>
2019-01-06 14:35:50 -0500

    Updated dist

--- dist/loader.js
+++ dist/loader.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = loader;
+exports.default = void 0;
 
 var _loaderUtils = _interopRequireDefault(require("loader-utils"));
 
@@ -15,10 +15,7 @@ var _options = _interopRequireDefault(require("./options.js"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
+// import path from 'path';
 function loader(source) {
   if (!source) {
     return "";
@@ -28,11 +25,20 @@ function loader(source) {
   (0, _schemaUtils.default)(_options.default, options, 'Generic JSX Loader');
 
   if (!options.serialize || !(options.serialize instanceof Function)) {
-    throw new Error("You must provide a serialize function as an option!");
+    throw new Error("You must provide a serialize function as an option! (generic-jsx-loader)");
   }
 
-  var parser = new _genericJsxTranspiler.Parser(_objectSpread({}, options));
+  var serialize = options.serialize;
+  var serializer = {
+    serialize: serialize
+  };
+  var parser = new _genericJsxTranspiler.Parser({
+    serializer: serializer
+  });
   return parser.parse({
     source: source
   }); //TODO support inputPath & outputPath
 }
+
+var _default = loader;
+exports.default = _default;
