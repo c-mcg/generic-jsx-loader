40c2356e88e733ef3a8c93b645c21f03ceeaa642
Nicole <niknakbakpak@gmail.com>
2019-01-06 15:55:57 -0500

    updated dist

--- dist/loader.js
+++ dist/loader.js
@@ -15,23 +15,26 @@ var _options = _interopRequireDefault(require("./options.js"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// import path from 'path';
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function loader(source) {
+  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
   if (!source) {
     return "";
   }
 
-  var options = _loaderUtils.default.getOptions(this) || {};
+  options = _objectSpread({}, options, _loaderUtils.default.getOptions(this) || {});
   (0, _schemaUtils.default)(_options.default, options, 'Generic JSX Loader');
 
   if (!options.serialize || !(options.serialize instanceof Function)) {
     throw new Error("You must provide a serialize function as an option! (generic-jsx-loader)");
   }
 
-  var serialize = options.serialize;
-  var serializer = {
-    serialize: serialize
-  };
+  var serializer = _objectSpread({}, options);
+
   var parser = new _genericJsxTranspiler.Parser({
     serializer: serializer
   });
